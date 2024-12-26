1c913e2a7b60399b102da7bb86624d36dad1f3f6
F8527576 Renan Jose De Souza Linhares <renanlinhares@bb.com.br>
2024-12-26 15:18:04 -0300

    BD

--- src/main/java/br/com/alura/domain/Endereco.java
+++ src/main/java/br/com/alura/domain/Endereco.java
@@ -1,7 +1,16 @@
 package br.com.alura.domain;
- 
+
+import io.quarkus.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Endereco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String rua;
     private String logradouro;
